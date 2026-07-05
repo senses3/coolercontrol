62a8429577c7e3cc55cd4ef4c5e4db809bf4d83f
Guy Boldon <gb@guyboldon.com>
2026-07-05 15:27:09 +0200

    feat(ui): vendor kit inputs for new shell

---
+++ coolercontrol-ui/src/shell/ui/UiCollapsible.vue
@@ -0,0 +1,51 @@
+<!--
+  - CoolerControl - monitor and control your cooling and other devices
+  - Copyright (c) 2021-2025  Guy Boldon and contributors
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  -->
+
+<script setup lang="ts">
+// @ts-ignore
+import SvgIcon from '@jamescoyle/vue-icon/lib/svg-icon.vue'
+import { mdiChevronRight } from '@mdi/js'
+import { CollapsibleContent, CollapsibleRoot, CollapsibleTrigger } from 'reka-ui'
+
+withDefaults(
+    defineProps<{
+        title: string
+        defaultOpen?: boolean
+    }>(),
+    { defaultOpen: false },
+)
+</script>
+
+<template>
+    <CollapsibleRoot :default-open="defaultOpen" class="w-full">
+        <CollapsibleTrigger
+            class="group flex w-full cursor-pointer items-center gap-1 rounded-lg px-2 py-1.5 text-sm font-medium text-text-color outline-none hover:bg-surface-hover focus-visible:ring-2 focus-visible:ring-accent"
+        >
+            <svg-icon
+                type="mdi"
+                :path="mdiChevronRight"
+                :size="16"
+                class="transition-transform group-data-[state=open]:rotate-90"
+            />
+            <span class="truncate">{{ title }}</span>
+        </CollapsibleTrigger>
+        <CollapsibleContent class="overflow-hidden">
+            <slot />
+        </CollapsibleContent>
+    </CollapsibleRoot>
+</template>
